66de64ada827c6f8c863bc2828688aac65798dc2
Marcos Barbosa <barbosa.mg@gmail.com>
2017-02-11 01:39:55 -0200

    Implementando delete

--- src/main/java/com/algaworks/vinhos/controller/VinhosController.java
+++ src/main/java/com/algaworks/vinhos/controller/VinhosController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,6 +65,15 @@ public class VinhosController {
 	public ModelAndView editar(@PathVariable Long codigo) {
 		Vinho vinho = vinhos.findOne(codigo);
 		return novo(vinho);
+	}
+	
+	@DeleteMapping("/{codigo}")
+	public String apagar(@PathVariable Long codigo, RedirectAttributes attributes) {
+		vinhos.delete(codigo);
+		
+		attributes.addFlashAttribute("mensagem", "Vinho removido com sucesso!");
+		
+		return "redirect:/vinhos";
 	}
 	
 }
